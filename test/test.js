84e2c6a75fff3052a07d69acf8e9609ee594a29a
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-29 13:08:20 +0300

    Use plugin options in error test method.

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const {readFileSync} = require('fs')
 const fixture = file => readFileSync(join(__dirname, 'fixtures', `${file}.html`), 'utf8')
 const expected = file => readFileSync(join(__dirname, 'expected', `${file}.html`), 'utf8')
 
-const error = (name, cb) => posthtml([plugin()]).process(fixture(name)).catch(cb) // eslint-disable-line
+const error = (name, options, cb) => posthtml([plugin(options)]).process(fixture(name)).catch(cb) // eslint-disable-line
 const clean = html => html.replace(/[^\S\r\n]+$/gm, '').trim()
 
 const process = (t, name, options, log = false) => {
